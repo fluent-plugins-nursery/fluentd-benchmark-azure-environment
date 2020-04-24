9807decd8506ba15157297ab61d44e68784cd500
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2020-04-24 19:48:10 +0900

    Tweak powershell parameter

--- ansible/config/bytes-message-bench.ps1
+++ ansible/config/bytes-message-bench.ps1
@@ -1,8 +1,11 @@
 Param(
-    [int32]$msg_length   # 数値
+    [string]$workdir = "C:\tools",
+    [parameter(mandatory=$true)][int32]$Length
 )
 
 $ENV:PATH="C:\opt\td-agent\embedded\bin;" + $ENV:PATH
+
+cd $workdir
 
 $fluentd_job = Start-Process fluentd -ArgumentList "-c", "C:\opt\td-agent\fluent-collector.conf", "-o", "C:\opt\td-agent\message-$msg_length-bytes.log" -NoNewWindow -PassThru
 
@@ -10,7 +13,7 @@ Start-Sleep 5
 
 $type_perf_job = Start-Process typeperf -ArgumentList "-cf", "counters.txt", "-sc", "2400", "-si", "1" -PassThru -RedirectStandardOutput C:\tools\${msg_length}-resource-usage.txt
 
-Start-Process C:\tools\EventLogBencher\EventLogBencher.exe -ArgumentList "-w", "50", "-t", "120000", "-l", "$msg_length" -Wait -NoNewWindow
+Start-Process C:\tools\EventLogBencher\EventLogBencher.exe -ArgumentList "-w", "50", "-t", "120000", "-l", "$Length" -Wait -NoNewWindow
 
 Stop-Process -Id $fluentd_job.Id
 Stop-Process -Id $type_perf_job.Id
