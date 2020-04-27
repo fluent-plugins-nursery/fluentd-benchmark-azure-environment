c38808e09304cdb09a9cd2f0c5db2cc077a16251
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2020-04-27 14:40:07 +0900

    Fix wrong variable usage

--- ansible/config/bytes-message-bench.ps1
+++ ansible/config/bytes-message-bench.ps1
@@ -8,11 +8,11 @@ $ENV:PATH="C:\opt\td-agent\embedded\bin;" + $ENV:PATH
 
 cd $workdir
 
-$fluentd_job = Start-Process fluentd -ArgumentList "-c", "C:\opt\td-agent\fluent-collector.conf", "-o", "C:\opt\td-agent\message-$msg_length-bytes.log" -NoNewWindow -PassThru
+$fluentd_job = Start-Process fluentd -ArgumentList "-c", "C:\opt\td-agent\fluent-collector.conf", "-o", "C:\opt\td-agent\message-$Length-bytes.log" -NoNewWindow -PassThru
 
 Start-Sleep 5
 
-$type_perf_job = Start-Process typeperf -ArgumentList "-cf", "counters.txt", "-sc", "2400", "-si", "1" -PassThru -RedirectStandardOutput C:\tools\${msg_length}-resource-usage.txt
+$type_perf_job = Start-Process typeperf -ArgumentList "-cf", "counters.txt", "-sc", "2400", "-si", "1" -PassThru -RedirectStandardOutput C:\tools\${Length}-resource-usage.txt
 
 Start-Process C:\tools\EventLogBencher\EventLogBencher.exe -ArgumentList "-w", "50", "-t", "$Total", "-l", "$Length" -Wait -NoNewWindow
 
