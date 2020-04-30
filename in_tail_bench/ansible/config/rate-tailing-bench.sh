01e35d97a7ceee1c934d1bd8ba151f5c51a1f434
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2020-04-30 16:58:23 +0900

    in_tail_bench: Implement in_tail scenerio benchmarking

---
+++ in_tail_bench/ansible/config/rate-tailing-bench.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+while getopts s:r: option
+do
+case "${option}"
+in
+s) STEP=${OPTARG};;
+r) RATE=${OPTARG};;
+esac
+done
+
+usage() {
+    echo "available options are:"
+    echo "-s STEP (for set up total step(s))"
+    echo "-r RATE (for set up generating line rate/sec)"
+    exit 1
+}
+
+if [ -z $STEP ]; then
+    echo "specify step with value -s VALUE."
+    usage
+fi
+
+if [ -z $RATE ]; then
+    echo "specify rate value with -r VALUE."
+    usage
+fi
+
+td-agent -c ${HOME}/td-agent.conf -o td-agent.log &
+
+sleep 3
+
+dummer -c ${HOME}/dummer/dummer.conf -r $RATE &
+
+python3 -u `which monitor` $STEP | tee usage-$RATE.tsv
+
+killall -TERM dummer
+killall -TERM ruby
+killall -TERM td-agent
