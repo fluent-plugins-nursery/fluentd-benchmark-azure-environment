dcfab497a52aff3bd686e770fbfdb3719868ad8e
Hiroshi Hatake <hatake@clear-code.com>
2020-06-17 15:49:30 +0900

    in_syslog_bench: Capture td-agent named process resource usage
    On Linux, supervisor process name is td-agent.
    
    Signed-off-by: Hiroshi Hatake <hatake@clear-code.com>

--- in_syslog_bench/ansible/config/monitor.py
+++ in_syslog_bench/ansible/config/monitor.py
@@ -68,19 +68,30 @@ recv bytes(/sec)\tsend bytes(/sec)", end='\t')
 
 steps = 1
 RUBY = "ruby"
+TD_AGENT = "td-agent"
 metrics = []
-pcount = 0
+ruby_process_count = 0
+td_agent_process_count = 0
 io_metrics = IOMetrics()
 
 for proc in psutil.process_iter():
     if proc.name() == RUBY:
         metric = ProcessMetrics(proc)
         metrics.append(metric)
-        CPU = "CPU Usage(%)#{0}".format(pcount)
-        RSS = "RSS(MB)#{0}".format(pcount)
-        VMS = "VMS(MB)#{0}".format(pcount)
+        CPU = "CPU Usage(%)[Ruby#{0}]".format(ruby_process_count)
+        RSS = "RSS(MB)[Ruby#{0}]".format(ruby_process_count)
+        VMS = "VMS(MB)[Ruby#{0}]".format(ruby_process_count)
         print(f"{CPU:8}\t{RSS:8}\t{VMS:8}", end="\t")
-        pcount = pcount + 1
+        ruby_process_count = ruby_process_count + 1
+
+    if proc.name() == TD_AGENT:
+        metric = ProcessMetrics(proc)
+        metrics.append(metric)
+        CPU = "CPU Usage(%)[TD-Agent#{0}]".format(td_agent_process_count)
+        RSS = "RSS(MB)[TD-Agent#{0}]".format(td_agent_process_count)
+        VMS = "VMS(MB)[TD-Agent#{0}]".format(td_agent_process_count)
+        print(f"{CPU:8}\t{RSS:8}\t{VMS:8}", end="\t")
+        td_agent_process_count = td_agent_process_count + 1
 
 print("")
 
