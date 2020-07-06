03f78ed2ab3f9869749ec1cba34700797cf7553f
Hiroshi Hatake <hatake@clear-code.com>
2020-07-06 18:56:38 +0900

    winevtlog_bench: Suppress deprecated warning
    Signed-off-by: Hiroshi Hatake <hatake@clear-code.com>

--- winevtlog_bench/network.tf
+++ winevtlog_bench/network.tf
@@ -56,7 +56,7 @@ resource "azurerm_subnet" "internal" {
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.fluentd.name
   virtual_network_name = azurerm_virtual_network.fluentd.name
-  address_prefix       = "10.0.2.0/24"
+  address_prefixes       = ["10.0.2.0/24"]
 }
 
 resource "azurerm_public_ip" "aggregator" {
